0b93f2ccc9e1222e83103dfc96925fddadd0b6de
Mathieu <TransLucid@seraphita.freeboxos.fr>
2018-10-03 14:54:29 +0200

    allocate with malloc

--- PDF_DeepZoom.c
+++ PDF_DeepZoom.c
@@ -3,7 +3,7 @@
  *
  *		Save a PDF file as a set of tiles at various resolutions with libvips. 
  * 		compile with:
- *		gcc -g -Wall -o PDF_DeepZoom PDF_DeepZoom.c `pkg-config vips --cflags --libs`
+ *		gcc -Wall -o PDF_DeepZoom PDF_DeepZoom.c `pkg-config vips --cflags --libs`
  * 		usage: ./PDF_DeepZoom pdfFile outputFolder
  * 		with pdfFile the pdf you want to process ant outputFolder the folder name where to store the result
  * 
@@ -15,14 +15,33 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int mat_len(char *s) 
+{ 
+	int i = 0; while(s[i]) i++; return i; 
+}
+
 int main( int argc, char **argv )
 {
 	VipsImage *in;
 	VipsImage *out;
 	
-	char path[100];
-	char thumb[104];
-	gdouble dpi=300;
+		
+	const char *prefix= "./public/images/dzi/";
+	const char *extension = ".png";
+	const gdouble dpi= 300;
+
+	char *path;
+	if((path=malloc(sizeof(path)*(strlen(prefix)+strlen(argv[2])+1)))==NULL)
+	{
+		exit (EXIT_FAILURE);
+	}
+
+	char *thumb;
+	if((thumb=malloc(sizeof(thumb)*(strlen(extension)+1)))==NULL)
+	{
+		exit (EXIT_FAILURE);
+	};
+
 	
 	if( VIPS_INIT( argv[0] ) )//open the vips library
 		vips_error_exit( NULL ); 
@@ -35,27 +54,23 @@ int main( int argc, char **argv )
 	
 	
 	//create folder name and folder
-	strcpy(path,"./public/images/dzi/");
+	strcpy(path,prefix);
 	strcat(path,argv[2]);
 	mkdir(path,0700);
 	strcat(path,"/");
 	strcat(path,argv[2]);
 	
-	//generate pyramide
+	
+	//generate tiles
 	if((vips_dzsave(in,path ,NULL))){
 		vips_error_exit( NULL );
 	}else{
 		printf("PDF created,moving temp files.\n");
 		
-		strcpy(thumb,argv[2]);
-		strcat(thumb,".png");
-		
-		strcpy(path,"");
-		strcpy(path,"./public/images/dzi/");
-		strcat(path,argv[2]);
-		strcat(path,"/");
+	
+		strcpy(thumb,extension);
 		strcat(path,thumb);
-		printf("path%s\n",path);
+		printf("thumbnail path:%s\n",path);
 			
 		//generate thumbnail
 		vips_thumbnail(argv[1],&out,128,NULL);
@@ -63,8 +78,14 @@ int main( int argc, char **argv )
 		if(!(vips_image_write_to_file(out,path,NULL)))
 			printf("Thumbnail created.\n");
 		
+	
 	}
 	vips_shutdown(); //close the vips library
-
+	
+	free(thumb);
+	free(path);
+	
 	return( 0 );
 }
+
+
